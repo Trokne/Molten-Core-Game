c9c031e25341f8bce73c89ff2dbb54d7659140db
Mikhail Vshivkov <m.vshivkov@xsolla.com>
2019-12-25 16:34:05 +0500

    Updates

---
+++ precache-manifest.ff535eb3e54c04f482d4894f572c842e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a17b082a865a4238f2d4fe3138f1ec",
+    "url": "/Molten-Core-Game/index.html"
+  },
+  {
+    "revision": "00c0e05a94e934bf92aa",
+    "url": "/Molten-Core-Game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8219f1975e8efc3627cd",
+    "url": "/Molten-Core-Game/static/js/2.4dc8e909.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Molten-Core-Game/static/js/2.4dc8e909.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00c0e05a94e934bf92aa",
+    "url": "/Molten-Core-Game/static/js/main.efe2011e.chunk.js"
+  },
+  {
+    "revision": "4343c5d5d5cf8ba4896e",
+    "url": "/Molten-Core-Game/static/js/runtime-main.4242ee94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Molten-Core-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
